57feff3178c1a2ae7b689cd2bd5285b08df11040
Gerard Lutterop <gerard.lutterop@bizservices.nl>
2023-10-24 14:43:22 +0200

    First commit. Get acces token and store it on the local machine.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# plesk-sitekick
-Connect  a Plesk server to the Sitekick domain analysis and monitoring services
+# Plesk - Sitekick
+Connect a Plesk server to the Sitekick domain analysis and monitoring services.
+
+## Introduction
+Sitekick is a domain analysis and monitoring service that provides a comprehensive overview of the health of your domain.
+Sitekick is used by major hosters for a number of applications: analysis, marketing, retention, and more.
+A major source of data is the actual hosting data, as it is provisioned by the hoster. One of the largest hosting
+platforms is Plesk. This module provides a simple way to connect a Plesk server to Sitekick in a secure way.
+
+## Installation
+
---
+++ send-data-to-sitekick.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Create a token for a Sitekick server, if it does not exist. This file can be executed every day to make sure that new
+Sitekick-servers are added and that depracated servers will be cleaned up.
+"""
+import datetime
+import json
+import subprocess
+import socket
+## getting the hostname by socket.gethostname() method
+hostname = socket.gethostname()
+## getting the IP address using socket.gethostbyname() method
+ip_address = socket.gethostbyname(hostname)
+
+def get_token(filename='/etc/plesk/tokens.json'):
+    """Get a token for local API access. If it was not generated, generate a new one and store it in a safe location."""
+    try:
+        with open(filename) as f:
+            tokens = json.loads(f.read())
+            return tokens[hostname]
+    except:
+        # No token stored for this server. Generate a new one and store it in a safe location.
+        # Get a token with the plesk bin secret_key tool and store it. First get the local IP-address:
+        proc = subprocess.run(["plesk", "bin", "secret_key", "-c", "-ip-address", ip_address, "-description",
+                               f'"Admin access token for {hostname} on {datetime.date.today()}"'],
+                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # Now the token is in the output of the command. Store it in a safe location:
+        token = proc.stdout.decode().strip()
+        try:
+            with open(filename) as f:
+                tokens = json.loads(f.read())
+        except:
+            tokens = {}
+        tokens[hostname] = token
+        with open(filename, 'w') as f:
+            f.write(json.dumps(tokens))
+

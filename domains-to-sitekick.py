6850ce0a313b8e504d77246ba17e3b6cc9a436a7
Gerard Lutterop <gerard.lutterop@bizservices.nl>
2023-11-02 17:17:22 +0100

    Dynamic loading of modules

--- domains-to-sitekick.py
+++ domains-to-sitekick.py
@@ -41,6 +41,9 @@ try:
 except NameError:
     __file__ = os.path.abspath('domains-to-sitekick.py')
 
+if sys.version_info < (3, 5):
+    print("Python 3.5 or higher is required")
+    sys.exit(1)
 
 def load_code(root_path=None):
     """Load the code from the sitekick server and store it in the code directory.
